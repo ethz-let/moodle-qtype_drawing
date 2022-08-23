26e54acaf02f397eecfc097a1fc9165a3bb6017e
Amr <amr.hourani@id.ethz.ch>
2022-08-23 11:24:24 +0200

    Update req version.

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_drawing';
 $plugin->version = 2022082300;
-$plugin->requires = 2021051707; // Moodle >=3.11+.
+$plugin->requires = 2021050100; // Moodle >=3.11+.
 
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '4 possible from Moodle 3.11 (Transitional)';
